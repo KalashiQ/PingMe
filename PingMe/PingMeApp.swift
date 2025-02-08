f7b56b69ace0579a24275a7705cf22fbc93c2973
Alexandr Khakana <Kalashiq.org@gmail.com>
2025-02-08 10:25:47 +0200

    Initial Commit

---
+++ PingMe/PingMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingMeApp.swift
+//  PingMe
+//
+//  Created by Александр on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PingMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PingMeTests/PingMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PingMeTests.swift
+//  PingMeTests
+//
+//  Created by Александр on 08.02.2025.
+//
+
+import Testing
+@testable import PingMe
+
+struct PingMeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
